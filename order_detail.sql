5660139221a7b6af622c472cbeb7e8ab49fbf3cf
Vivian0936 <89381239+Vivian0936@users.noreply.github.com>
2024-04-20 14:21:31 +0800

    Add files via upload

---
+++ order_detail.sql
@@ -0,0 +1,16 @@
+-- This is to show order_details
+-- Within a specific order, we can display all the item details
+-- param (order_id,)
+
+use `proj`;
+
+SELECT `order`.`order_id`, `item`.`item_name`,`order_item`.`item_id`,`order`.`store_id`,
+    `order_item`.`quantity`,`order`.`customer_id`,
+    `order`.`order_time`,`order`.`order_status`,
+    `item`.`item_price`,`item`.`item_type`,`item`.`item_inventory`
+FROM `order_item`
+LEFT JOIN `order`
+ON `order_item`.`order_id` = `order`.`order_id`
+RIGHT JOIN `item`
+ON `order_item`.`item_id` = `item`.`item_id`
+WHERE `order`.`order_id` = %s
---
+++ store_detail.sql
@@ -0,0 +1,11 @@
+-- This is to show certain store details (items in the store)
+-- param (store_id,)
+
+WITH tmp AS (
+	SELECT *
+	FROM `store`
+	WHERE store_id = %s
+)
+SELECT tmp.*, item.*
+FROM tmp
+JOIN item ON tmp.store_id = store.store_id
