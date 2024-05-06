c558e551eb4a5f3e8aaae5c59d765afc22a5ed46
1lycoris <130223765+1lycoris@users.noreply.github.com>
2024-05-06 23:44:30 +0800

    Add files via upload

--- second_ed/query/order_detail.sql
+++ second_ed/query/order_detail.sql
@@ -4,7 +4,7 @@
 
 use `proj`;
 
-SELECT `order`.`order_id`, `item`.`item_name`,`order_item`.`item_id`,`order`.`store_id`,
+SELECT `order`.`order_id`, `item`.`item_name`,`order_item`.`item_id`,`store`.`store_name`,
     `order_item`.`quantity`,`order`.`customer_id`,
     `order`.`order_time`,`order`.`order_status`,
     `item`.`item_price`,`item`.`item_type`,`item`.`item_inventory`
@@ -13,4 +13,6 @@ LEFT JOIN `order`
 ON `order_item`.`order_id` = `order`.`order_id`
 RIGHT JOIN `item`
 ON `order_item`.`item_id` = `item`.`item_id`
+INNER JOIN `store`
+ON `order_item`.`store_id` = `store`.`store_id`
 WHERE `order`.`order_id` = %s
